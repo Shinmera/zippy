eee1f79dba308c11f4f9c30e3986269cd5981be8
Shinmera <shinmera@tymoon.eu>
2020-07-14 13:37:02 +0200

    remove obsolete

--- io.lisp
+++ io.lisp
@@ -67,22 +67,3 @@
             value))
          (stream
           (,(intern (format NIL "~a-~a" 'read structure-type)) ,input))))))
-
-(defun call-with-io (function io &key (direction :input) (if-exists :error) (index 0))
-  (etypecase io
-    ((or pathname string)
-     (handler-bind ((archive-file-required
-                      (lambda (c)
-                        (let ((id (slot-value c 'id)))
-                          ;; FIXME: Leaking FDs
-                          (use-value (open (make-pathname :type (format NIL "z~2,'0d" (1+ id)) :defaults io)
-                                           :element-type '(unsigned-byte 8)))))))
-       (with-open-file (stream io :direction direction :if-exists if-exists :element-type '(unsigned-byte 8))
-         (funcall function stream))))
-    (stream
-     (funcall function io))
-    (vector
-     (funcall function (make-vector-input io index)))))
-
-(defmacro with-io ((io target &rest args) &body body)
-  `(call-with-io (lambda (,io) ,@body) ,target ,@args))
