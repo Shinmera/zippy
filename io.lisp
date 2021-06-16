17942603125675489584c810b05d01d9195fe3c0
Shinmera <shinmera@tymoon.eu>
2021-06-16 23:24:19 +0200

    Dumb.

--- io.lisp
+++ io.lisp
@@ -10,7 +10,7 @@
   `(or stream vector-input directory-input))
 
 (defstruct (vector-input (:constructor make-vector-input (vector index start end)))
-  (vector NIL :type (simple-array (unsigned-byte 8) *) :read-only T)
+  (vector NIL :type (simple-array (unsigned-byte 8) (*)) :read-only T)
   (start 0 :type fixnum :read-only T)
   (end 0 :type fixnum :read-only T)
   (index 0 :type fixnum))
